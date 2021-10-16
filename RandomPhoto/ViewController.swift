b4d28c52a39f77d74aee96cab80159db329e93e1
이우석 <leewoosuk@iuseog-ui-MacBookAir.local>
2021-10-17 02:07:01 +0900

    color change

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by 이우석 on 2021/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
